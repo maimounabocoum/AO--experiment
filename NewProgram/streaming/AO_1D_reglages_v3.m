00f40682dfda9616dc68b8b4f5282fa3f72c2247
maimouna bocoum <maimouna.bocoum@gmail.com>
2017-03-08 22:18:29 +0100

    test duration of acquisition

---
+++ NewProgram/streaming/AO_1D_reglages_v3.m
@@ -0,0 +1,238 @@
+%% Script for AO setup optimization. To be used with Aixplorer and Gage
+%% acquisition card.
+% Display and refresh an acousto-optic line
+%
+% Created by Clement on 10/06/2015
+% Last modified : Clement 10/06/2015
+
+%% LegHal for Aixplorer
+
+addpath('D:\legHAL')
+addPathLegHAL;
+
+%% Gage folders
+
+addpath('C:\Program Files (x86)\Gage\CompuScope\CompuScope MATLAB SDK\CsMl')
+addpath('AcqGage\');
+
+%% Parameters
+
+Nloop = 100;
+% US Parameters
+Volt        = 50; % V
+FreqSonde   = 6;  % MHz
+NbHemicycle = 6;
+X0          = 20; % mm
+Foc         = 30; % mm
+NTrig       = 1000;
+Prof        = 50; % mm
+verbose      = false;
+%Acquisition parameters
+Range = 1; % V
+SampleRate = 10; % MHz
+
+% Save
+Sauvegarde =0;
+Date = datestr(now,'yyyy-mm-dd');
+path = 'D:\Data\Clement\In Vivo\Bras JB';
+Name = 'Poignet JB';
+
+%% Security check
+if FreqSonde ==15 && Volt>25
+    Volt=25;
+    warning('Tension max dépassée ! Limite 25 V');
+end
+
+
+%% Set Aixplorer parameters
+AixplorerIP    = '192.168.1.16'; % IP address of the Aixplorer device
+
+NoOp       = 500;             % µs minimum time between two US pulses
+
+%% Paramètres de la séquence US
+% tension etc...
+ImgVoltage = Volt;             % imaging voltage [V]
+ImgCurrent = 0.1;            % security limit for imaging current [A]
+PushVoltage = 25;             % imaging voltage [V]
+PushCurrent = 5;            % security limit for imaging current [A]
+
+% ============================================================================ %
+
+% Image info
+ImgInfo.LinePitch = 1;  % dx / PiezoPitch
+ImgInfo.PitchPix  = 0.5;  % dz / lambda
+ImgInfo.Depth(1)  = 2;   % initial depth [mm]
+ImgInfo.Depth(2)  = 20;  % final depth [mm]
+ImgInfo.RxApod(1) = 0.4; % RxApodOne
+ImgInfo.RxApod(2) = 0.6; % RxApodZero
+
+% ============================================================================ %
+
+% Emission
+CP.TwFreq     = FreqSonde;       % MHz 15
+CP.NbHcycle   = NbHemicycle ;         %
+CP.ApodFct    = 'none';    %
+CP.DutyCycle  = 1;         %
+CP.TxCenter   = 0;        % mm
+CP.TxWidth    = Foc/2;        % mm
+
+
+CP.PosZ = Foc; % mm
+CP.PosX = X0; % mm
+% ============================================================================ %
+
+% Acquisition
+CP.RxFreq       = 60; %4 * CP.TwFreq; % MHz
+CP.RxCenter     = 0;          % mm
+CP.RxWidth      = 32;          % mm - no synthetic acquisition
+CP.RxBandwidth  = 2;          % 1 = 200%, 2 = 100%
+
+% ============================================================================ %
+
+% Sequence execution
+NbLocalBuffer = 1; % # of local buffer
+NbHostBuffer  = 1; % # of host buffer
+NbRun         = 1; % # of sequence executions
+RepeatElusev  = 1; % # of acmo repetition before dma
+
+% ============================================================================ %
+
+% DO NOT CHANGE - Estimate dedicated variables for UltraColor
+NumSamples   = ceil(1.4 * (2 * ImgInfo.Depth(2) - ImgInfo.Depth(1)) * 1e3 ...
+    * CP.RxFreq / (common.constants.SoundSpeed * 2^(CP.RxBandwidth-1) * 128)) * 128;
+SkipSamples  = floor(0.6 * ImgInfo.Depth(1) * CP.RxFreq * 1e3 ...
+    / common.constants.SoundSpeed);
+
+% DO NOT CHANGE - Estimate dedicated variables for Ultrafast
+CP.RxDuration = NumSamples / CP.RxFreq * 2^(CP.RxBandwidth-1);
+CP.RxDelay    = SkipSamples / CP.RxFreq;
+% ======================================================================= %
+%% Build the sequence
+% =========================================================================
+
+FOCUSED = elusev.focused( ...
+    'TwFreq', CP.TwFreq, ...
+    'NbHcycle', CP.NbHcycle, ...
+    'Polarity', 1, ...
+    'SteerAngle', 0, ...
+    'Focus', CP.PosZ, ...    %en mm
+    'Pause', NoOp, ...       % Debut de la pause entre chaque tir (µs)
+    'PauseEnd', CP.NbHcycle/(2*CP.TwFreq*0.002),... %200, ...       % Fin de la pause (µs)
+    'DutyCycle', 1, ...
+    'TxCenter', CP.PosX, ...
+    'TxWidth', CP.TxWidth, ...
+    'RxFreq', CP.RxFreq, ...
+    'RxCenter', CP.RxCenter, ...
+    'RxDuration', 8.5333, ...
+    'RxDelay', 0, ...
+    'RxBandwidth', 2, ...
+    'FIRBandwidth', 0, ...
+    'PulseInv', 0, ...
+    'TrigOut', 50, ...%slogP.AcqTime , ...    %en µs
+    'TrigAll', 0, ...
+    'Repeat', NTrig+1, ...
+    'ApodFct', 'none', ...
+    0);
+
+% Acousto ACMO
+ACOUSTO = acmo.acmo( ...
+    'Repeat', 1, ...
+    'Duration', 0, ...
+    'ControlPts', 900, ...
+    'fastTGC', 0, ...
+    'NbLocalBuffer', 0, ...
+    'NbHostBuffer', 1, ...
+    'RepeatElusev', 1, ...
+    0);
+
+ACOUSTO = ACOUSTO.setParam('elusev',FOCUSED,'Ordering',[1]);
+
+clear 'FOCUSED'
+
+% ============================================================================ %
+
+% Create sequence
+TPC = remote.tpc( ...
+    'imgVoltage', ImgVoltage, ...
+    'imgCurrent', ImgCurrent, ...
+    'pushVoltage', PushVoltage, ...
+    'pushCurrent', PushCurrent, ...
+    0);
+SEQ = usse.usse( ...
+    'TPC', TPC, ...
+    'acmo', ACOUSTO, ...
+    'Repeat', NbRun, ...
+    'Loop', 0, ...
+    'DropFrames', 0, ...
+    0);
+
+clear 'ACOUSTO'
+clear 'TPC'
+% ============================================================================ %
+% Build the REMOTE structure
+[SEQ NbAcq] = SEQ.buildRemote();
+
+
+%% Initialize Gage Acquisition card
+[ret,handle] = InitGage_1D_reglages_v3(NTrig,Prof,SampleRate,Range);
+
+% [ret, acqInfo] = CsMl_QueryAcquisition(handle);
+% CsMl_ErrorHandler(ret, 1, handle);
+% [ret, sysinfo] = CsMl_GetSystemInfo(handle); % Get card infos
+% CsMl_ErrorHandler(ret, 1, handle);
+% 
+% CsMl_ResetTimeStamp(handle);
+
+
+% % Set transfer parameters
+% transfer.Mode           = CsMl_Translate('Default', 'TxMode');
+% transfer.Start          = 0;
+% transfer.Length         = acqInfo.SegmentSize;
+% 
+% MaskedMode              = bitand(acqInfo.Mode, 15); % Check acq. mode
+% ChannelsPerBoard        = sysinfo.ChannelCount / sysinfo.BoardCount; % get number of channels
+% ChannelSkip             = ChannelsPerBoard / MaskedMode; % number of channels that are skipped during
+% % the transfer step.
+
+
+
+
+%% Acquisition AO
+SEQ = SEQ.initializeRemote('IPaddress', AixplorerIP);
+SEQ = SEQ.loadSequence();
+
+%% Acquire data
+for k = 1:Nloop
+    
+SEQ = SEQ.startSequence();
+[ret,upData] = Mex_StartStreaming(handle,verbose);
+CsMl_ErrorHandler(ret, 1, handle);
+
+
+SEQ = SEQ.stopSequence('Wait', 0);
+
+%  data in Volts
+upData =(double(upData)).*2*acqInfo.Range/(2^16);
+
+data = mean(upData,2);
+
+    Z = linspace(0,Prof,acqInfo.Depth);
+    
+    
+    figure (42)
+    plot(Z,data)
+    xlabel ('Depth (mm)')
+    ylabel ('Signal (V)')
+    drawnow
+%     data_end(:,k)=data;
+    fullDate = datestr(now,'yyyy-mm-dd_MM_ss');
+
+     if(Sauvegarde == 1)
+        mkdir([path '\' Date]);
+        save([path '\' Date '\' Name '-' fullDate '.mat'],'Z','data','raw', 'Volt','FreqSonde',...
+                    'NbHemicycle', 'X0', 'Foc', 'NTrig', 'Prof', 'Range', 'SampleRate');
+    end
+end
+
+
+SEQ = SEQ.quitRemote();
---
+++ NewProgram/streaming/InitGage_1D_reglages_v3.m
@@ -0,0 +1,122 @@
+function [ret,handle] = InitGage_1D_reglages(NTrig,Prof,SamplingRate,Range)
+% Set the acquisition, channel and trigger parameters for the system and
+% commit the parameters to the driver.
+% AO     = structure with the different parameters
+% ret    = positive is setup is successful, negative corresponding to the
+% error code else.
+% handle = handle for the acq. card
+%
+% To be used with AO_1D_reglages.m
+%
+% Created by Clement on 10/06/2015
+% Last modified : Clement on 10/06/2015
+
+
+
+systems = CsMl_Initialize; % Initialize driver
+CsMl_ErrorHandler(systems);
+
+
+[ret, handle] = CsMl_GetSystem; % Get ready to use Gage handle
+try
+    % If error, try to free cards and try to get handle again
+    CsMl_ErrorHandler(ret);
+    
+catch exception
+    warning(exception.message,exception.identifier)
+    display('Attempting to free system')
+    ret=CsMl_FreeAllSystems; %frees all systems
+    
+    try
+        CsMl_ErrorHandler(ret); % If no error, system freed
+        display('System Freed')
+        
+    catch exception
+        %else, clear all and return
+        warning(exception.message,exception.identifier)
+        clear all
+        return
+    end
+    
+    [ret, handle] = CsMl_GetSystem; %try to get handle again
+    
+    try
+        CsMl_ErrorHandler(ret); % If no error, got handle
+        
+    catch exception
+        %else, clear all and return
+        errordlg(exception.message,exception.identifier)
+        clear all
+        return
+    end
+end
+
+[ret, sysinfo] = CsMl_GetSystemInfo(handle); % Get card infos
+CsMl_ErrorHandler(ret, 1, handle);
+
+s = sprintf('-----Board name: %s\n', sysinfo.BoardName);
+disp(s);
+
+% ======================================================================= %
+%% Acquisition parameters
+ 
+%Customed Parameters
+
+acqInfo.SampleRate      = SamplingRate*1e6;%Max = 50 MHz, must be divider of 50;
+acqInfo.SegmentCount    = NTrig; % Number of memory segments 
+%acqInfo.Depth           = ceil((acqInfo.SampleRate*1e-3*Prof/(common.constants.SoundSpeed))/32)*32; % Must be a multiple of 32
+acqInfo.Depth           = ceil((acqInfo.SampleRate*1e-6*ceil(Prof/(common.constants.SoundSpeed*1e-3)))/32)*32; % Must be a multiple of 32
+
+
+%"Do not change" Parameters
+acqInfo.ExtClock        = 0;
+acqInfo.Mode            = CsMl_Translate('Single', 'Mode');% Use only one channel
+acqInfo.SegmentSize     = acqInfo.Depth; % Must be a multiple of 32
+acqInfo.TriggerTimeout  = 2e6; % in µs
+acqInfo.TriggerHoldoff  = 0; % Number of points during which the card ignores trigs
+acqInfo.TriggerDelay    = 0; % Number of points
+acqInfo.TimeStampConfig = 1; % Get the time at which each waveform was acquired
+% if non-zero, restart the time stamp counter at each acq.
+
+[ret] = CsMl_ConfigureAcquisition(handle, acqInfo);% config acq parameters
+CsMl_ErrorHandler(ret, 1, handle);
+
+% initializes all the channels even though
+% they might not all be used.
+for i = 1:sysinfo.ChannelCount
+    chan(i).Channel     = i;
+    chan(i).Coupling    = CsMl_Translate('DC', 'Coupling');
+    chan(i).DiffInput   = 0;
+    chan(i).InputRange  = 2000;%2000; Vpp in mV
+    chan(i).Impedance   = 50; % 50 Ohms or 1 MOhms
+    chan(i).DcOffset    = 0;
+    chan(i).DirectAdc   = 0;
+    chan(i).Filter      = 0; 
+end   
+
+% Sets channel 1
+chan(1).Coupling        = CsMl_Translate('DC', 'Coupling');
+chan(1).InputRange      = 2*Range*1e3; % Vpp in mV
+
+[ret] = CsMl_ConfigureChannel(handle, chan); % config chan parameters
+CsMl_ErrorHandler(ret, 1, handle);
+
+% Sets trigger parameters
+trig.Trigger            = 1;
+trig.Slope              = CsMl_Translate('Negative', 'Slope'); % Aixplorer Trig has a neg slope
+trig.Level              = 10; % in percent of the trig range (-100 to +100)
+trig.Source             = CsMl_Translate('External', 'Source');
+trig.ExtCoupling        = CsMl_Translate('DC', 'ExtCoupling');
+trig.ExtRange           = 10000; % Vpp in mV, 10000=+-5V
+
+[ret] = CsMl_ConfigureTrigger(handle, trig); % config Trig parameters
+CsMl_ErrorHandler(ret, 1, handle);
+
+ret = CsMl_Commit(handle);
+CsMl_ErrorHandler(ret, 1, handle);
+
+%% Set stream mode
+verbose = true;
+ret = Mex_SetStreamMode(handle,verbose);
+CsMl_ErrorHandler(ret, 1, handle);
+ret = 1;
