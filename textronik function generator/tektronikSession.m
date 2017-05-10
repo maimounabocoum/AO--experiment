7de192a8b7473fe6778bd045c5afd2aee7339fd4
Maimouna Bocoum <maimouna.bocoum@gmail.com>
2017-05-10 17:56:06 +0200

    last

--- textronik function generator/tektronikSession.m
+++ textronik function generator/tektronikSession.m
@@ -1,48 +1,54 @@
-%TEKTRONIKSESSION Code for communicating with an instrument.
-%
-%   This is the machine generated representation of an instrument control
-%   session. The instrument control session comprises all the steps you are
-%   likely to take when communicating with your instrument. These steps are:
-%   
-%       1. Create an instrument object
-%       2. Connect to the instrument
-%       3. Configure properties
-%       4. Write and read data
-%       5. Disconnect from the instrument
-% 
+%TEKTRONIKSESSION Code for communicating with an instrument. 
+%  
+%   This is the machine generated representation of an instrument control 
+%   session using a device object. The instrument control session comprises  
+%   all the steps you are likely to take when communicating with your  
+%   instrument. These steps are:
+%       
+%       1. Create a device object   
+%       2. Connect to the instrument 
+%       3. Configure properties 
+%       4. Invoke functions 
+%       5. Disconnect from the instrument 
+%  
 %   To run the instrument control session, type the name of the file,
 %   tektronikSession, at the MATLAB command prompt.
 % 
-%   The file, TEKTRONIKSESSION.M must be on your MATLAB PATH. For additional information 
-%   on setting your MATLAB PATH, type 'help addpath' at the MATLAB command 
+%   The file, TEKTRONIKSESSION.M must be on your MATLAB PATH. For additional information
+%   on setting your MATLAB PATH, type 'help addpath' at the MATLAB command
 %   prompt.
-% 
+%
 %   Example:
-%       tektroniksession;
-% 
-%   See also SERIAL, GPIB, TCPIP, UDP, VISA, BLUETOOTH, I2C, SPI.
-% 
- 
-%   Creation time: 10-May-2017 17:02:03
+%       tektronikSession;
+%
+%   See also ICDEVICE.
+%
 
-% Find a VISA-USB object.
-obj1 = instrfind('Type', 'visa-usb', 'RsrcName', 'USB0::0x0699::0x0343::C021048::0::INSTR', 'Tag', '');
+%   Creation time: 10-May-2017 17:19:55 
+
+
+% Create a VISA-USB object.
+interfaceObj = instrfind('Type', 'visa-usb', 'RsrcName', 'USB0::0x0699::0x0343::C021048::0::INSTR', 'Tag', '');
 
 % Create the VISA-USB object if it does not exist
 % otherwise use the object that was found.
-if isempty(obj1)
-    obj1 = visa('NI', 'USB0::0x0699::0x0343::C021048::0::INSTR');
+if isempty(interfaceObj)
+    interfaceObj = visa('NI', 'USB0::0x0699::0x0343::C021048::0::INSTR');
 else
-    fclose(obj1);
-    obj1 = obj1(1)
+    fclose(interfaceObj);
+    interfaceObj = interfaceObj(1);
 end
 
-% Connect to instrument object, obj1.
-fopen(obj1);
+% Create a device object. 
+deviceObj = icdevice('tek_afg3000.mdd', interfaceObj);
 
-% Disconnect all objects.
-fclose(obj1);
+% Connect device object to hardware.
+connect(deviceObj);
 
-% Clean up all objects.
-delete(obj1);
+% Execute device object function(s).
+groupObj = get(deviceObj, 'System');
+invoke(groupObj, 'loadstate');
+geterror(deviceObj)
 
+% Delete objects.
+delete([deviceObj interfaceObj]);
