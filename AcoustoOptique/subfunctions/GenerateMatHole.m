5cc7c39ae529d6e2cc24d0539693a88a9f5a2431
Maimouna Bocoum <maimouna.bocoum@gmail.com>
2017-07-05 11:55:56 +0200

    removing useless files

--- AcoustoOptique/subfunctions/GenerateMatHole.m
+++ AcoustoOptique/subfunctions/GenerateMatHole.m
@@ -1,21 +1,23 @@
-function Mat = CalcMatHole(f0,f,nuX,X);
+function Mat = GenerateMatHole( f0,fz,nuX,X )
+
 % f0 (en MHz) est la fréquence de porteuse,  f (en Hz) la fréquence de modulation de phase
 
 Tmax = 20.1;  % durée maximale d'un cycle élémentaire (en us)
 Fe = 180;     % Fréquence d'échantillonnage en MHz
 
-T = 1e6/f;
-if (T>Tmax) % si la période de la modulation de phase est plus grande que Tmax --> Manip impossible
+dt = 1e6/fz;  % time step in us
+
+if ( dt  > Tmax ) % si la période de la modulation de phase est plus grande que Tmax --> Manip impossible
    Mat = zeros(size(X));
    return;
 end;
 
-N = floor(Tmax/T); %on essaie de se rapprocher au mieux de Tmax
-Nech = round(N*T*Fe); % Nombre d'échantillons effectivement utilisés
+N = floor(Tmax/dt);     %   on essaie de se rapprocher au mieux de Tmax
+Nech = round(N*dt*Fe);  %   Nombre d'échantillons effectivement utilisés
 
 Tot = Nech/Fe; % durée totale de la séquence à répéter 
 
-  if (T ~= Tot/N)
+  if (dt ~= Tot/N)
       d = 999
   end
 
@@ -28,11 +30,15 @@ k = floor(Tot*f0); % nombre de cycles porteuse
   end
 f0 = k/Tot; % ré-ajustement de la fréquence porteuse
 
-t = (0:Nech-1)/180; % Echelle de temps;
-s0 = sin(2*pi*f0*t); %porteuse
+t = (0:Nech-1)/180;         %   Echelle de temps;
+        
 tT = t'*ones(1,length(X));
 tX = nuX*ones(length(t),1)*X/f;
-ts0 = s0'*ones(1,length(X));
 
-Mat = sign(ts0).*(sin(2*pi*f*(tT-tX))>0);
-    
+ts0 = sin(2*pi*f0*t)'*ones(1,length(X)); %   porteuse
+
+Mat = sign(ts0).*( sin(2*pi*fz*(tT-tX)) > 0 );
+
+
+end
+
