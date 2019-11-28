c67ae39097480e18e79c5dd51a505a1522432abc
Maimouna Bocoum <maimouna.bocoum@gmail.com>
2019-11-28 17:16:05 +0100

    sdv

--- AcoustoOptique/AcoustoOptiqueDATA_ANALYSES.m
+++ AcoustoOptique/AcoustoOptiqueDATA_ANALYSES.m
@@ -8,54 +8,43 @@
  %% =============== load datas ===============  %%
  
  % data : 13/11/2019:
- importdata('Q:\datas\2019-11-08\Signal_modeTwicking_RepHz_100_17h32_35.mat')
+ % importdata('Q:\datas\2019-11-08\Signal_modeTwicking_RepHz_100_17h32_35.mat')
  
  
  %% =============== screen out datas ===============  %%
- PmuW
+
      h = 6.6e-34;
      lambda = 780e-9;
      Ephoton = h*(3e8/lambda);
- 
+     Fs1 = 10e6 ;
+     Fs2 = 100 ;
+     % definition of 2 structure for data analyses:
+     MyStat1 = stats_t( Fs1 );
+     MyStat2 = stats_t( Fs2 );
+     
     Hmu = figure(1);
     % set(Hmu,'WindowStyle','docked');
-     
-    % raw/(0.45*1e5) unit = W
-    [Datas_mu1,Datas_std1,Datas_mu2,Datas_std2] = AverageDataBothWays( raw/(0.45*1e5) );
-
-    mu_short = mean(1e6*Datas_mu1)
-    mu_long = mean(1e6*Datas_mu2)
-    Sig_short = mean(1e6*Datas_std1)
-    Sig_long = mean(1e6*Datas_std2)
     
-% mu   = [5.5134   11.4693 14.7709 17.6676  17.9219 22.5880 25.4040 29.4386 35.6482] ;
-% sig1 = [0.0454 0.0493  0.0520  0.0553  0.0551  0.0723  0.0766  0.0807  0.0881] ;
-% sig2 = [0.0488 0.0949  0.0978 0.0997   0.1574  0.1299  0.1366  0.1950  2.2718] ;
-%    
-%    figure(2);
-%    plot(mu,sig1)
-%    hold on
-%    plot(mu,sig2)
-%    legend({'short','long'})
-%    xlabel('power(\mu W)')
-%    ylabel('\sigma (\mu W)')
-%    hold off 
+    % raw/(0.45*1e5)  [V]unit x [W/V] = [W]unit
     
+    [t1,Datas_mu1]   = MyStat1.average( raw/(0.45*1e5) );
+    Datas_std1  = MyStat1.standard_dev( raw/(0.45*1e5) ) ;
+    [t2,Datas_mu2]   = MyStat2.average( raw'/(0.45*1e5) );
+    Datas_std2  = MyStat2.standard_dev( raw'/(0.45*1e5) );
+    
+   % [Datas_mu1,Datas_std1,Datas_mu2,Datas_std2] = AverageDataBothWays( raw/(0.45*1e5) );
+   
     subplot(221)
-    imagesc(1e6*raw/(0.45*1e5))
+imagesc(1e6*raw/(0.45*1e5))
     xlabel('index long')
     ylabel('index short')
     cb = colorbar;
     ylabel(cb,'\mu W')
     colormap(parula)
-   
-    
-
-    
-    
+  
     subplot(223)
-    line((1:length(Datas_std1))*1e-2,1e6*Datas_std1,'Color','r'); hold on 
-    line((1:length(Datas_std1))*1e-2,1e6*sqrt(Ephoton*(10e6)*Datas_mu1),'Color','r'); hold off
+line( t1*1e3,1e6*Datas_std1,'Color','r'); hold on 
+line( t1*1e3,1e6*sqrt(Ephoton*Fs1*Datas_mu1),'Color','r'); hold off
     ylabel('\sigma (\mu W) over short ')
     xlabel('time (ms)')
     ylim([0 2])
@@ -67,7 +56,7 @@
     'XAxisLocation','top',...
     'YAxisLocation','right',...
     'Color','none');
-    line(t,1e6*Datas_std2,'Parent',ax2,'Color','k')
+line(t2*1e6,1e6*Datas_std2,'Parent',ax2,'Color','k')
     set(ax2,'Ylim',get(ax1,'Ylim'));
     ylabel('\sigma (nW) over long')
     xlabel('time (\mu s)')
@@ -75,7 +64,7 @@
 
     
     subplot(222)
-    line((1:length(Datas_std1))*1e-2,1e6*Datas_mu1,'Color','r'); hold on 
+line(t1*1e3,1e6*Datas_mu1,'Color','r'); hold on 
     ylabel('\mu (\mu W)over short ')
     xlabel('time (ms)')
     ax1 = gca; % current axes
@@ -86,15 +75,25 @@
     'XAxisLocation','top',...
     'YAxisLocation','right',...
     'Color','none');
-    line(t,1e6*Datas_mu2,'Parent',ax2,'Color','k')
+line(t2*1e6,1e6*Datas_mu2,'Parent',ax2,'Color','k')
     set(ax2,'Ylim',get(ax1,'Ylim'));
     ylabel('\mu (\mu W)over  long')
     xlabel('time (\mu s)')
 
     subplot(224)
-    [freq1 , psdx1 ] = CalculateDoublePSD( raw , 10e6 );
-    [freq2 , psdx2 ] = CalculateDoublePSD( raw' , 100e3 );
-    line(freq2*1e-3,10*log10(100e3*psdx2),'Color','r')
+[freq1 , psdx1 ] = MyStat1.PowSpecDens( raw/(0.45*1e5) ) ;
+% psdx1 = mean( psdx1 , 2 ) ;
+[freq2 , psdx2 ] = MyStat2.PowSpecDens( raw'/(0.45*1e5) ) ;
+psdx2 = mean( psdx2 , 2 ) ;
+
+e1  = MyStat1.Energy_t( raw/(0.45*1e5) );
+ee1 = MyStat1.Energy_psd( psdx1 ); 
+figure; plot(e1) ; hold on ; plot(ee1)
+
+
+% [freq1 , psdx1 ] = CalculateDoublePSD( raw  , Fs1 );
+% [freq2 , psdx2 ] = CalculateDoublePSD( raw' , Fs2 );
+line(freq2*1e-3,10*log10(100e3*psdx2),'Color','r')
     ylim([-52 -40])
     xlabel('Frequency (kHz)')
     ylabel('Power (dB)')
@@ -106,7 +105,7 @@ ax2 = axes('Position',ax1_pos,...
     'XAxisLocation','top',...
     'YAxisLocation','right',...
     'Color','none');
-    line(freq1*1e-6,10*log10(10e6*psdx1),'Parent',ax2,'Color','k')
+line(freq1*1e-6,10*log10(10e6*psdx1),'Parent',ax2,'Color','k')
     set(ax2,'Ylim',get(ax1,'Ylim'));
     xlabel('Frequency (MHz)')
     ylabel('Power (dB)')
