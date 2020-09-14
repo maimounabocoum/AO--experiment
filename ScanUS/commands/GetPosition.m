a9bccf66edc99fe28f01a76ee42cf4668b910f41
bocoum <maimouna.bocoum@gmail.com>
2020-09-14 10:50:31 +0200

    After Vacations

--- ScanUS/commands/GetPosition.m
+++
@@ -1,7 +0,0 @@
-function y = GetPosition(controleur,MyMotor)
-
-fprintf(controleur,[MyMotor ' np']);
-y = str2double(fscanf(controleur,'%s'));
-
-% print result
-disp([' Current Position (mm) ' , num2str(y) ])
